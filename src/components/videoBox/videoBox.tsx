64fcf31bc504d710c18f4cc561b34ad40702bc93
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-01-13 15:47:53 +0100

    removing comments

--- src/components/videoBox/videoBox.tsx
+++ src/components/videoBox/videoBox.tsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import "./videoBox.css"
-
-// export const VideoBox = () => {
-
-//   return (
-//     <div className="media">
-//         <div>
-//             <img src="logo192.png" alt="Logo" />
-//         </div>
-//       <h3>
-//         Title of the video
-//       </h3>
-//       <span>
-//         Description
-//       </span>
-//     </div>
-//   );
-// };
-
-// export default VideoBox;
-
 import React from "react";
 import "./videoBox.css"
 
