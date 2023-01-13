fb87fe66ee125691735a7d90da28821479b735f5
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-01-13 22:38:12 +0100

    added pagination on homepage

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,16 +5,17 @@ import axios from 'axios';
 import VideoBox from "./videoBox/videoBox";
 import CurrentVideo from "./currentVideo/currentVideo";
 import { Video } from "../../types";
-
+import "./home.css"
 
 const Home: FC = () => {
+  const [page, setPage] = useState(1);
   const [videos, setVideos] = useState<Array<Video> | undefined>(undefined);
+  console.log("🚀 ~ file: home.tsx:13 ~ videos", videos)
   const [currentVideo, setCurrentVideo] = useState<Video | undefined>(undefined);
-  console.log("🚀 ~ file: App.tsx:27 ~ currentVideo", currentVideo)
 
   const getVideos = async () => {
       try {
-          const response = await axios.get('http://localhost:3000/api/videos/all');
+          const response = await axios.get(`http://localhost:3000/api/videos/all?page=${page}`);
           setVideos(response.data)
       } catch (error) {
           console.error(error);
@@ -27,7 +28,7 @@ const Home: FC = () => {
 
   useEffect(() => {
     getVideos()
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -39,6 +40,17 @@ const Home: FC = () => {
           {videos && videos.map((video: Video) =>
             <VideoBox key={video._id} video={video} selectCurrentVideo={selectCurrentVideo} />)
           }
+          <div className="centerButtons">
+            {videos && page < 3 &&
+              <button onClick={() => setPage(page + 1)}>
+                Next videos
+              </button>
+            }
+            {videos && page > 1 && 
+            <button onClick={() => setPage(page - 1)} style={{marginRight: "20px"}}>
+              Previous page
+            </button>}
+          </div>
         </div>
         <div style={{margin: "0 5%"}}>
         {currentVideo && 
