a4187d752bd0f5804b0175e43a15a17352a3efb1
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-01-13 16:17:22 +0100

    fetching videos from backend and showing them

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
 import React from "react";
+import axios from 'axios';
+
 import VideoBox from "./components/videoBox/videoBox"
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // import { Routes, Route, Link } from "react-router-dom";
+interface Video {
+  _id: string;
+  author: string;
+  // "questions": [
+  //     {
+  //         "questionId": "63be6d861e7a6a473b600b7b",
+  //         "time": 11
+  //     },
+  //     {
+  //         "questionId": "63be6d861e7a6a473b600b7c",
+  //         "time": 131
+  //     }
+  // ],
+  title: string;
+  videoId: string;
+}
 
 const App: React.FC = () => {
   // const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined);
+  const [videos, setVideos] = useState<Array<Video> | undefined>(undefined);
 
-  // useEffect(() => {
-  // }, []);
+  const getVideos = async () => {
+      try {
+          const response = await axios.get('http://localhost:3000/api/videos/all');
+          setVideos(response.data)
+      } catch (error) {
+          console.error(error);
+      }
+  }
+
+  useEffect(() => {
+    getVideos()
+  }, []);
 
   return (
     <div>
       <nav>
           EDPUZZLE
       </nav>
-      <VideoBox title="Title" description="Description" imageUrl="logo192.png" />
+      {videos && videos.map((video: Video) =>
+        <VideoBox key={video._id} title={video.title} imageUrl="videoPreview.jpg" />
+      )
+      }
     </div>
   );
 };
--- src/components/videoBox/videoBox.tsx
+++ src/components/videoBox/videoBox.tsx
@@ -3,17 +3,15 @@ import "./videoBox.css"
 
 interface Props {
   title: string;
-  description: string;
   imageUrl: string;
 }
 
-export const VideoBox: React.FC<Props> = ({title, description, imageUrl}) => {
+export const VideoBox: React.FC<Props> = ({title, imageUrl}) => {
     return (
       <div className="video-preview">
         <img src={imageUrl} alt={title}/>
         <div className="video-info">
           <h3>{title}</h3>
-          <p>{description}</p>
         </div>
       </div>
     );
