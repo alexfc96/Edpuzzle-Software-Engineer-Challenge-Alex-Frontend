fd0b2d06c03f08914a05929116626fdaa9e20bdd
alexfc96 <alex.fernandez.canovas@gmail.com>
2023-01-16 20:54:20 +0100

    refactoring behaviour of home page

--- src/components/home/allVideos/allVideos.tsx
+++ src/components/home/allVideos/allVideos.tsx
@@ -1,58 +1,53 @@
-import React, {FC} from "react";
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import React, { FC } from "react";
+import { useState } from "react";
 
 import VideoInfoBox from "../videoInfoBox/videoInfoBox";
 import SelectedVideo from "../selectedVideo/selectedVideo";
 import { Video } from "../../../types";
-import "./allVideos.css"
+import "./allVideos.css";
 
-const AllVideos: FC = () => {
-  const [page, setPage] = useState(1);
-  const [videos, setVideos] = useState<Array<Video> | undefined>(undefined);
-  const [selectedVideo, setSelectedVideo] = useState<Video | undefined>(undefined);
+interface Props {
+  videos: Video[];
+  setPage: (page: number) => void;
+  page: number;
+}
+
+const AllVideos: FC<Props> = ({ videos, setPage, page }) => {
+  const [selectedVideo, setSelectedVideo] = useState<Video | undefined>(
+    undefined
+  );
 
   const selectVideo = (video: Video) => {
     setSelectedVideo(video);
-  }
-
-  const getVideos = async () => {
-    try {
-        const response = await axios.get(`http://localhost:3000/api/videos/all?page=${page}`);
-        setVideos(response.data)
-    } catch (error) {
-        console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    getVideos()
-  }, [page]);
+  };
 
   return (
     <>
-    <div>
-        {videos && videos.map((video: Video) =>
-          <VideoInfoBox key={video._id} video={video} selectVideo={selectVideo} />)
-        }
+      <div>
+        {videos.map((video: Video) => (
+          <VideoInfoBox
+            key={video._id}
+            video={video}
+            selectVideo={selectVideo}
+          />
+        ))}
         <div className="centerButtons">
-          {videos && page < 3 &&
-            <button onClick={() => setPage(page + 1)}>
-              Next videos
-            </button>
-          }
-          {videos && page > 1 &&
-            <button onClick={() => setPage(page - 1)} style={{marginRight: "20px"}}>
+          {page < 3 && (
+            <button onClick={() => setPage(page + 1)}>Next videos</button>
+          )}
+          {page > 1 && (
+            <button
+              onClick={() => setPage(page - 1)}
+              style={{ marginRight: "20px" }}
+            >
               Previous page
             </button>
-          }
+          )}
         </div>
-    </div>
-    <div className="marginVideo">
-      {selectedVideo && 
-        <SelectedVideo video={selectedVideo} />
-      }
-    </div>
+      </div>
+      <div className="marginVideo">
+        {selectedVideo && <SelectedVideo video={selectedVideo} />}
+      </div>
     </>
   );
 };
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,18 +1,37 @@
-import React, {FC} from "react";
+import React, {FC, useEffect, useState} from "react";
 
 import AllVideos from "./allVideos/allVideos";
 import "./home.css"
 import NavbarLayout from "../navbarLayout/navbarLayout";
+import { Video } from "../../types";
+import axios from "axios";
 
 const Home: FC = () => {
+  const [videos, setVideos] = useState<Array<Video> | undefined>(undefined);
+  const [page, setPage] = useState(1);
+
+  const getVideos = async () => {
+    try {
+        const response = await axios.get(`http://localhost:3000/api/videos/all?page=${page}`);
+        setVideos(response.data)
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getVideos()
+  }, [page]);
+
   return (
     <div>
       <NavbarLayout />
-      <div className="homeFlex" style={{display: "flex", marginTop: "2%", marginLeft: "5%"}}>
-        <AllVideos />
+      <div className="homeFlex">
+        {videos && <AllVideos videos={videos} page={page} setPage={setPage} />}
+        {!videos && <h2>No videos at this moment. Try again later.</h2>}
       </div>
     </div>
   );
 };
 
-export default Home
+export default Home;
